cc5c2b59176885a1f1aa6549e96a447b3599bb15
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-05-12 12:25:14 -0400

    Update Misc.h

--- Journey-of-the-gods/Misc.h
+++ Journey-of-the-gods/Misc.h
@@ -1,4 +1,4 @@
-//For misc. functions and stuff.
+using namespace std; //For misc. functions and stuff.
 int gen(int x, int y) //Generates a random number between x and y.
 {
 	return rand()%(y-x+1)+x;
