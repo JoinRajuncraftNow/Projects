deda91c19d6039b5deef045880c7a69f49b70481
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-05-12 10:25:30 -0400

    Update Player.h

--- Journey-of-the-gods/Player.h
+++ Journey-of-the-gods/Player.h
@@ -1,17 +1,25 @@
 //All inventory, health, and other stuff.
-void up(int& y) //Don't remember if I can do this here, so I will just assume I can until I can test it.
+void forward(int& y) //Don't remember if I can do this here, so I will just assume I can until I can test it.
 {
-  
+  y++;
 }
-void down(int& y)
+void forward(int& y)
 {
-  
+  y--;
 }
 void left(int& x)
 {
-  
+  x--;
 }
 void right(int& x)
 {
-  
+  x++;
+}
+void up(int& z)
+{
+  z++;
+}
+void down(int& z)
+{
+  z--;
 }
