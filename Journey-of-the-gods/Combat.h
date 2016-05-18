e799afeaad77bab69ea47cc98269a52f0f9bbc9e
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-05-18 14:30:11 -0400

    Update Combat.h

--- Journey-of-the-gods/Combat.h
+++ Journey-of-the-gods/Combat.h
@@ -15,3 +15,21 @@ void damage(int& damage, int& health)
 {
   health-=damage;
 }
+//Spells:
+int fireball() //Deals medium damage and has a chance of lighting the enemy on fire.
+{
+  
+}
+int freeze() //Deals medium damage and has a chance of freezing the enemy.
+{
+  
+}
+int godwrath() //Deals large amounts of damage, but will rebound and damage the caster.
+{
+  
+}
+int vampire() //Deals no damage, but just sucks a small amount of life out of enemies.
+{
+  
+}
+//Going to consult with creative people for more.
