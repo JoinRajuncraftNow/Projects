46c91ae6d718cc37cdbfa3cac7501c72d51c05f6
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-07-02 18:31:53 -0500

    Update Combat.h

--- Journey-of-the-gods/Combat.h
+++ Journey-of-the-gods/Combat.h
@@ -6,6 +6,7 @@ void attack(string name, char raceid, string race, int& z, int& x, int& y, int& 
 int adamage(int primary, int secondary, int attacktype)
 {
   switch(primary)
+  {
   case 1:
   //Short sword 10-20 base damage
   break;
@@ -21,7 +22,7 @@ int adamage(int primary, int secondary, int attacktype)
   default: 
   //Fists 1-5 base damage
   break;
-  //I think this is how you use switch and stuff, although I have not been over that in a while.
+  }
 }
 void death()
 {
