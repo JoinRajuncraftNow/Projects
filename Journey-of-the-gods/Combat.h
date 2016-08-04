723f4374cb16f31d8117076e43cffd421f4671a5
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-08-04 10:06:55 -0400

    Update Combat.h

--- Journey-of-the-gods/Combat.h
+++ Journey-of-the-gods/Combat.h
@@ -8,20 +8,20 @@ int adamage(int primary, int secondary, int attacktype)
   switch(primary)
   {
   case 1:
-  //Short sword 10-20 base damage
-  break;
+    //Short sword 10-20 base damage
+    break;
   case 2:
-  //Longsword 20-40 base damage
-  break;
+    //Longsword 20-40 base damage
+    break;
   case 3:
-  //Club 2-10 base damage
-  break;
+    //Club 2-10 base damage
+    break;
   case 4:
-  //Spear 5-25 base damage
-  break;
+    //Spear 5-25 base damage
+    break;
   default: 
-  //Fists 1-5 base damage
-  break;
+    //Fists 1-5 base damage
+    break;
   }
 }
 void death()
