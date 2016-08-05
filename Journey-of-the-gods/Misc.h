0015a8d8cd2c38090b3be7fbde1571d7986bebbf
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-08-05 10:07:26 -0400

    Update Misc.h

--- Journey-of-the-gods/Misc.h
+++ Journey-of-the-gods/Misc.h
@@ -3,3 +3,27 @@ int gen(int x, int y) //Generates a random number between x and y.
 {
 	return rand()%(y-x+1)+x;
 }
+
+//MAP FUNCTIONS
+int mapgen()
+{
+	cout << "Generating Map..." << endl; //Randomly generates map.
+	while(x<100)
+	{
+		while(y<100)
+		{
+			while(z<5)
+			{
+				map[x][y][z]=gen(1,10);
+				z++;
+			}
+			y++;
+			z=0;
+		}
+		x++;
+		y=0;
+	}
+	map[99][99][0]=11;
+	x=0; //y is already 0!
+	z=4;
+}
