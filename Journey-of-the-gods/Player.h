2094cc0e1354fa24791b252b5e991df056cb4953
Zacharý <zacharyacutey@users.noreply.github.com>
2016-08-09 16:11:04 -0400

    Update Player.h

--- Journey-of-the-gods/Player.h
+++ Journey-of-the-gods/Player.h
@@ -1,6 +1,26 @@
 using namespace std; //All inventory, health, and other stuff.
-extern killcount, x, y, z, health, magic, helm, chest, leggings, boots, quest, enemyid, ehealth, ehelm, echest, eleggings, eboots, damage, primary, secondary, raceid;
-extern map[100][100][5];
+extern int killcount;
+extern int x;
+extern int y;
+extern int z;
+extern int health;
+extern int magic;
+extern int helm;
+extern int chest;
+extern int leggings;
+extern int boots;
+extern int quest;
+extern int enemyid;
+extern int ehealth;
+extern int ehelm;
+extern int echest;
+extern int eleggings;
+extern int eboots;
+extern int damage;
+extern int primary;
+extern int secondary;
+extern int raceid;
+extern int map[100][100][5];
 void forward(int& y) //Don't remember if I can do this here, so I will just assume I can until I can test it.
 {
   y++;
