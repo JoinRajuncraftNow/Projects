8cf15a76e95ec9830eaff7ce6a03ecc26c564c71
Zacharý <zacharyacutey@users.noreply.github.com>
2016-08-09 16:13:28 -0400

    Update Quests.h

--- Journey-of-the-gods/Quests.h
+++ Journey-of-the-gods/Quests.h
@@ -1,3 +1,23 @@
 using namespace std; //All Quest-related stuff
-extern killcount, x, y, z, health, magic, helm, chest, leggings, boots, quest, enemyid, ehealth, ehelm, echest, eleggings, eboots, damage, primary, secondary, raceid;
-extern map[100][100][5];
+extern int killcount;
+extern int x;
+extern int y;
+extern int z;
+extern int health;
+extern int magic;
+extern int helm;
+extern int chest;
+extern int leggings;
+extern int boots;
+extern int quest;
+extern int enemyid;
+extern int ehealth;
+extern int ehelm;
+extern int echest;
+extern int eleggings;
+extern int eboots;
+extern int damage;
+extern int primary;
+extern int secondary;
+extern int raceid;
+extern int map[100][100][5];
