72fc28b275bf4561fd41ef85973abd357b25e705
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-08-09 10:03:55 -0400

    Update Player.h

--- Journey-of-the-gods/Player.h
+++ Journey-of-the-gods/Player.h
@@ -1,4 +1,6 @@
 using namespace std; //All inventory, health, and other stuff.
+extern killcount, x, y, z, health, magic, helm, chest, leggings, boots, quest, enemyid, ehealth, ehelm, echest, eleggings, eboots, damage, primary, secondary, raceid;
+extern map[100][100][5];
 void forward(int& y) //Don't remember if I can do this here, so I will just assume I can until I can test it.
 {
   y++;
