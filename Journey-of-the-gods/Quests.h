ea083848e136aef59bdc2fb623f50622f4da3837
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-08-09 10:04:08 -0400

    Update Quests.h

--- Journey-of-the-gods/Quests.h
+++ Journey-of-the-gods/Quests.h
@@ -1,1 +1,3 @@
 using namespace std; //All Quest-related stuff
+extern killcount, x, y, z, health, magic, helm, chest, leggings, boots, quest, enemyid, ehealth, ehelm, echest, eleggings, eboots, damage, primary, secondary, raceid;
+extern map[100][100][5];
