d299b29f5026ecbb8f15833a0fa0b38b14d02508
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-08-24 11:25:23 -0400

    Update Combat.h

--- Journey-of-the-gods/Combat.h
+++ Journey-of-the-gods/Combat.h
@@ -25,7 +25,7 @@ extern int primary;
 extern int secondary;
 extern int raceid;
 extern string enemy;
-extern int map[1000][1000][5];
+extern int map[200][200][3];
 void combat() //This is where the combat will be.
 {
   enemyid=gen(1, 5);
