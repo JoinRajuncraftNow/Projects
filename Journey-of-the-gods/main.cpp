99e35139d72a11caa46aea3d5b0b01e36713f600
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-08-24 11:19:10 -0400

    Update main.cpp

--- Journey-of-the-gods/main.cpp
+++ Journey-of-the-gods/main.cpp
@@ -4,7 +4,7 @@ using namespace std; //This might fix it
 #include <string>
 #include <cstdlib>
 #include <ctime>
-#include <stdlib.h>
+#include <windows.h> //wrong one!
 #include "conio.h"
 #include "Combat.h"
 #include "Quests.h"
