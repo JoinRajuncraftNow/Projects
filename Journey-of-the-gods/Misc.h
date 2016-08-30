c24e9cd1bd434c07e6f922c386e0940c78374a25
Zacharý <zacharyacutey@users.noreply.github.com>
2016-08-30 12:04:04 -0400

    Update Misc.h

--- Journey-of-the-gods/Misc.h
+++ Journey-of-the-gods/Misc.h
@@ -316,22 +316,22 @@ void mapgen()
 					case 7: //trading station
 						weapon[x][y][z]=1; //If someone upgrades their weapon at a trading station, this is how much damage is added to their current weapon. It is set to 0 once the player buys it.
 						armor[x][y][z]=1; //Same as weapon but for armor
-						medkit[x][y][z]=gen(2,3); //How many medkits this place has to buy
+						smedkit[x][y][z]=gen(2,3); //How many medkits this place has to buy
 						break;
 					case 8: //city
 						weapon[x][y][z]=gen(8,10);
 						armor[x][y][z]=gen(8,10);
-						medkit[x][y][z]=gen(15,20);
+						smedkit[x][y][z]=gen(15,20);
 						break;
 					case 9: //town
 						weapon[x][y][z]=gen(4,6);
 						armor[x][y][z]=gen(4,6);
-						medkit[x][y][z]=gen(8,13);
+						smedkit[x][y][z]=gen(8,13);
 						break;
 					case 10: //settlement
 						weapon[x][y][z]=gen(2,4);
 						armor[x][y][z]=gen(2,4);
-						medkit[x][y][z]=gen(3,8);
+						smedkit[x][y][z]=gen(3,8);
 						break;
 					default:
 						while(1)
