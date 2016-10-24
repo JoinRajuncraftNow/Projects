478017e60bbace584c4ac04195ff0ab6a89073bf
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-10-24 10:29:56 -0400

    Update inout.h

--- 2048/inout.h
+++ 2048/inout.h
@@ -1,1 +1,26 @@
-
+void command()
+{
+	char c;
+	switch(c=(char)getch())
+	{
+	case 'w':
+	case 'W':
+		up();    //key up (W)
+		break;
+	case 's':
+	case 'S':
+		down();   // key down (S)
+		break;
+	case 'a':
+	case 'A':
+		left();  // key left (A)
+		break;
+	case 'd':
+	case 'D':
+		right();  // key right (D)
+		break;
+	default:
+		cout << endl << "Unknown Command." << endl;  // not valid
+		break;
+	}
+}
