fc847c97e08a923702c100f532d6fbf815f77dec
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-10-24 10:46:55 -0400

    Update main.cpp

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -4,7 +4,32 @@
 #include <ctime>
 #include "conio.h"
 using namespace std;
+int gen(int d, int g) //Generates a random number between x and y.
+{
+	return rand()%(g-d+1)+d; //OUTPUTS ONLY 6!!! WTF
+}
 int main()
 {
 	int board[4][4];
+	int x=0,y=0;
+	//BOARD GENERATION
+	while(y<4)
+	{
+		while(x<4)
+		{	
+			board[x][y]=0;
+			if(gen(1,16)==1) //1\16 chance of getting a number other than 0.
+			{
+				if(gen(1,3)==1) //1\3 chance of 4
+				{
+					board[x][y]=4;
+				}else{
+					board[x][y]=2;
+				}
+			}
+			x++;
+		}
+		y++;
+		x=0;
+	}
 }
