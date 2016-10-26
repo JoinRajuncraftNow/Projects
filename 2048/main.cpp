683b538805667ecf34439d0342b1ce7b7b11e1fa
JoinRajuncraftNow <thecajunthatrages@gmail.com>
2016-10-26 11:19:12 -0400

    Update main.cpp

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -9,10 +9,11 @@ int board[4][4];
 using namespace std;
 int gen(int d, int g) //Generates a random number between x and y.
 {
-	return rand()%(g-d+1)+d; //OUTPUTS ONLY 6!!! WTF
+	return rand()%(g-d+1)+d;
 }
 int main()
 {
+	srand(time(NULL));
 	int x=0,y=0;
 	//BOARD GENERATION
 	while(y<4)
@@ -20,7 +21,7 @@ int main()
 		while(x<4)
 		{	
 			board[x][y]=0;
-			if(gen(1,16)==1) //1\16 chance of getting a number other than 0.
+			if(gen(1,24)==1) //1\16 chance of getting a number other than 0.
 			{
 				if(gen(1,3)==1) //1\3 chance of 4
 				{
@@ -34,9 +35,20 @@ int main()
 		y++;
 		x=0;
 	}
+	for(int i=0; i<1; i++)
+	{
+		x=gen(0,3);
+		y=gen(0,3);
+		if(gen(1,3)==1) //1\3 chance of 4
+		{
+			board[x][y]=4;
+		}else{
+			board[x][y]=2;
+		}
+	}
 	while(1)
 	{
 		boardout();
-		cin >> x;
+		return 0;
 	}
 }
